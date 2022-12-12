363cda71c4bd098a8bebc453d6e09bdd227894f6
Wasim Shhab <wasem20aboshhab@gmail.com>
2022-12-12 23:58:56 +0200

    full background img

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter,NavLink,Route, Routes} from "react-router-dom"
+import LiveResults from "./LiveResults";
+import LeagueTable from "./LeagueTable";
+import LeagueTableLive from "./LeagueTableLive";
+import Login from "./Login";
+
+
+
 
 function App() {
-  return (
+    const links=[
+        // {to:"/", text:"home page"},
+        {to:"Login", text:"Login"},
+        {to:"LiveResults", text:"LiveResults"},
+        {to:"LeagueTableLive", text:"LeagueTableLive"},
+        {to:"LeagueTable", text:"LeagueTable"},
+    ]
+
+    // const activeMenuClass=({isActive})=>(isActive ? "active-menu" : "non-active-menu");
+
+    return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <BrowserRouter>
+          <ul>
+              {
+                  links.map((link)=>{
+                      return(
+                          <button className={"Buttons"}>
+                              <NavLink
+                                  to={link.to}>{link.text}</NavLink>
+                          </button>
+                      )
+                  })
+              }
+          </ul>
+
+          <Routes>
+            <Route path={"/Login"} element ={<Login/>}/>
+            <Route path={"/LiveResults"} element ={<LiveResults/>}/>
+            <Route path={"/LeagueTableLive"} element = {<LeagueTableLive/>}/>
+            <Route path={"/LeagueTable"}  element={<LeagueTable/>}/>
+        </Routes>
+      </BrowserRouter>
+
+
     </div>
   );
 }
